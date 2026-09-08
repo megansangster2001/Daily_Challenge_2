d39b4c9f3a01745a0dae6151b808ec22e6e52204
Megan <269440206+megansangster2001@users.noreply.github.com>
2026-09-08 12:34:55 +0100

    TDD player X logic

---
+++ app/src/main/java/org/example/TicTacToe.java
@@ -0,0 +1,77 @@
+package org.example;
+
+class TicTacToe {
+
+    private String currentPlayer = "X";
+    private String [] board = new String[9];
+    private String winner;
+
+
+    public String getCurrentPlayer() {
+        return currentPlayer;
+    }
+    public void makeMove(int position) {
+        if (position < 1 || position > 9) {
+            return;
+        }
+        if (board[position - 1] == null) {
+            String player = currentPlayer;
+            board[position - 1] = player;
+
+
+            if(hasWinner()) {
+                winner = player;
+            }
+
+            if (currentPlayer.equals("X")) {
+                currentPlayer = "O";
+            } else {
+                currentPlayer = "X";
+            }
+        }
+    }
+
+    public String getPosition(int position){
+            return board[position - 1];
+    }
+    public boolean hasWinner() {
+        return board[0] != null &&
+                board[0].equals(board[1]) &&
+                board[1].equals(board[2])
+                ||
+                (board[3] != null &&
+                        board[3].equals(board[4]) &&
+                        board[4].equals(board[5]))
+
+                ||
+                (board[6] != null &&
+                        board[6].equals(board[7]) &&
+                        board[7].equals(board[8]))
+                ||
+                (board[0] != null &&
+                        board[0].equals(board[3]) &&
+                        board[3].equals(board[6]))
+                ||
+                (board[1] != null &&
+                        board[1].equals(board[4]) &&
+                        board[4].equals(board[7]))
+
+                ||
+                (board[2] != null &&
+                        board[2].equals(board[5]) &&
+                        board[5].equals(board[8]))
+                ||
+                (board[0] != null &&
+                        board[0].equals(board[4]) &&
+                        board[4].equals(board[8]))
+                ||
+                (board[2] != null &&
+                        board[2].equals(board[4]) &&
+                        board[4].equals(board[6]));
+
+
+    }
+    public String getWinner() {
+        return winner;
+    }
+}
---
+++ app/src/test/java/org/example/TicTacToeTest.java
@@ -0,0 +1,216 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TicTacToeTest {
+
+    //X starts the game
+
+    @Test
+    void gameStartsWithPlayerX() {
+        TicTacToe game = new TicTacToe();
+        assertEquals("X", game.getCurrentPlayer());
+    }
+
+    //X - O after a move
+
+    @Test
+    void turnChangestoPlayerOOncePlayerXTakesTheirTurn() {
+        TicTacToe game = new TicTacToe();
+
+        game.makeMove(1);
+
+        assertEquals("O", game.getCurrentPlayer());
+    }
+
+    //O - X after a move
+
+    @Test
+    void turnChangesbacktoPlayerXOncePlayerOHasTakenTheirTurn() {
+        TicTacToe game = new TicTacToe();
+
+        game.makeMove(1);
+        game.makeMove(2);
+
+        assertEquals("X", game.getCurrentPlayer());
+    }
+
+    //A player can claim a position
+
+    @Test
+    void PlayerXCanClaimAPosition() {
+        TicTacToe game = new TicTacToe();
+
+        game.makeMove(1);
+
+        assertEquals("X", game.getPosition(1));
+    }
+
+    //An occupied position cannot be claimed
+
+    @Test
+    void PlayerCannotClaimAnOccupiedPosition() {
+        TicTacToe game = new TicTacToe();
+
+        game.makeMove(1);
+        game.makeMove(1);
+
+        assertEquals("X", game.getPosition(1));
+    }
+
+    //Cannot make move outside the board (only 9 positions)
+    @Test
+    void CannotMakeMoveOutsideBoard() {
+        TicTacToe game = new TicTacToe();
+
+        game.makeMove(10);
+
+        assertEquals("X", game.getCurrentPlayer());
+    }
+
+    //Checking position 1-9 on board all work correctly
+
+    @Test
+    void PlayerCanClaimDifferentPositons() {
+        TicTacToe game = new TicTacToe();
+
+        game.makeMove(1);
+        game.makeMove(2);
+
+        assertEquals("X",game.getPosition(1));
+
+        assertEquals("O",game.getPosition(2));
+    }
+
+
+    //Checking Player X can win with top row
+
+    @Test
+    void PlayerXCanWithTopRow() {
+        TicTacToe game = new TicTacToe();
+            game.makeMove(1); //X
+            game.makeMove(7); //O
+            game.makeMove(2); //X
+            game.makeMove(5); //O
+            game.makeMove(3); //X
+
+        assertTrue(game.hasWinner());
+    }
+
+    //Checking Player X can win with middle row
+
+    @Test
+    void PlayerXCanWithMiddleRow() {
+        TicTacToe game = new TicTacToe();
+        game.makeMove(4); //X
+        game.makeMove(3); //O
+        game.makeMove(5); //X
+        game.makeMove(1); //O
+        game.makeMove(6); //X
+
+        assertTrue(game.hasWinner());
+    }
+    //Checking Player X can win with bottom row
+
+    @Test
+    void PlayerXCanWithBottomRow() {
+        TicTacToe game = new TicTacToe();
+        game.makeMove(7); //X
+        game.makeMove(3); //O
+        game.makeMove(8); //X
+        game.makeMove(1); //O
+        game.makeMove(9); //X
+
+        assertTrue(game.hasWinner());
+    }
+
+    //Checking Player X can win with right column
+
+    @Test
+    void PlayerXCanWithRightColumn() {
+        TicTacToe game = new TicTacToe();
+        game.makeMove(1); //X
+        game.makeMove(3); //O
+        game.makeMove(4); //X
+        game.makeMove(2); //O
+        game.makeMove(7); //X
+
+        assertTrue(game.hasWinner());
+    }
+
+    //Checking Player X can win with middle column
+
+    @Test
+    void PlayerXCanWithMiddleColumn() {
+        TicTacToe game = new TicTacToe();
+        game.makeMove(2); //X
+        game.makeMove(3); //O
+        game.makeMove(5); //X
+        game.makeMove(4); //O
+        game.makeMove(8); //X
+
+        assertTrue(game.hasWinner());
+    }
+    //Checking Player X can win with left column
+
+    @Test
+    void PlayerXCanWithLeftColumn() {
+        TicTacToe game = new TicTacToe();
+        game.makeMove(3); //X
+        game.makeMove(1); //O
+        game.makeMove(6); //X
+        game.makeMove(4); //O
+        game.makeMove(9); //X
+
+        assertTrue(game.hasWinner());
+    }
+
+    //Checking Player X can win with diagonal
+
+    @Test
+    void PlayerXCanWithDiagonal() {
+        TicTacToe game = new TicTacToe();
+        game.makeMove(1); //X
+        game.makeMove(2); //O
+        game.makeMove(5); //X
+        game.makeMove(4); //O
+        game.makeMove(9); //X
+
+        assertTrue(game.hasWinner());
+    }
+
+    //Checking Player X can win with reverse diagonal
+
+    @Test
+    void PlayerXCanWithReverseDiagonal() {
+        TicTacToe game = new TicTacToe();
+        game.makeMove(3); //X
+        game.makeMove(2); //O
+        game.makeMove(5); //X
+        game.makeMove(4); //O
+        game.makeMove(7); //X
+
+        assertTrue(game.hasWinner());
+    }
+
+    //checking X is the winner
+
+    @Test
+    void PlayerXIsTheWinner() {
+        TicTacToe game = new TicTacToe();
+
+        game.makeMove(1); // X
+        game.makeMove(4); // O
+        game.makeMove(2); // X
+        game.makeMove(5); // O
+        game.makeMove(3); // X
+
+        assertEquals("X", game.getWinner());
+
+    }
+
+}
